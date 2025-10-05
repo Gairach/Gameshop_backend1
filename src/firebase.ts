38bcd5ddf8a03d20306c7be3f627f34c8d9db407
ShuCha <cursees07@gmail.com>
2025-10-05 21:30:28 +0700

    1

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,14 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
 
+dotenv.config(); // โหลดค่า .env
+
+// ตรวจสอบ environment variable
+if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_CLIENT_EMAIL || !process.env.FIREBASE_PRIVATE_KEY) {
+  throw new Error("Missing Firebase environment variables");
+}
+
+// Initialize Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
